98d68c540751bce225c0e70d27a0b91ca93628c3
Hao <xionghaocoding@163.com>
2018-01-19 09:32:39 +0800

    shader program

---
+++ LearnOpenGL--Shader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnOpenGL--Shader
+//
+//  Created by Mac OS X on 2018/1/18.
+//  Copyright © 2018年 Mac OS X. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
